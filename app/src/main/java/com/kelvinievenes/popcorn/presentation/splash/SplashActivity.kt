191f46ef2e9de9dbe4f5aeb9523e0f30603b4783
Kelvao <kelvin.ievenes@fatec.sp.gov.br>
2019-11-09 20:54:26 -0300

    Feat: splash screen architeture improved, main activity and bottom navigation view added

--- app/src/main/java/com/kelvinievenes/popcorn/presentation/splash/SplashActivity.kt
+++ app/src/main/java/com/kelvinievenes/popcorn/presentation/splash/SplashActivity.kt
@@ -1,18 +1,19 @@
-package com.kelvinievenes.popcorn.presentation
+package com.kelvinievenes.popcorn.presentation.splash
 
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.kelvinievenes.popcorn.R
+import com.kelvinievenes.popcorn.presentation.main.view.MainActivity
 import com.kelvinievenes.popcorn.presentation.movielist.view.MovieListActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_splash)
         Handler().postDelayed({
-            startActivity(MovieListActivity.getStartIntent(this))
+            startActivity(MainActivity.getStartIntent(this))
             finish()
         }, 1000)
     }
