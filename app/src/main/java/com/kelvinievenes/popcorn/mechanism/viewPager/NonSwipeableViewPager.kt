c4f3314951c73d95619356e0073c8cc2c26c80d5
Kelvao <kelvin.ievenes@fatec.sp.gov.br>
2019-11-10 02:11:34 -0300

    Feat: custom view pager added to best control of fragments

---
+++ app/src/main/java/com/kelvinievenes/popcorn/mechanism/viewPager/NonSwipeableViewPager.kt
@@ -0,0 +1,16 @@
+package com.kelvinievenes.popcorn.mechanism.viewPager
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.viewpager.widget.ViewPager
+
+class NonSwipeableViewPager(
+    context: Context,
+    attrs: AttributeSet?
+) : ViewPager(context, attrs) {
+
+    override fun onTouchEvent(ev: MotionEvent?): Boolean = false
+
+    override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean = false
+}
--- app/src/main/java/com/kelvinievenes/popcorn/presentation/main/view/MainActivity.kt
+++ app/src/main/java/com/kelvinievenes/popcorn/presentation/main/view/MainActivity.kt
@@ -4,45 +4,33 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.core.view.forEachIndexed
 import com.kelvinievenes.popcorn.R
-import com.kelvinievenes.popcorn.presentation.favorites.view.FavoritesFragment
-import com.kelvinievenes.popcorn.presentation.movielist.view.MovieListFragment
+import com.kelvinievenes.popcorn.presentation.main.view.adapter.ViewPagerAdapter
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var movieListFragment: MovieListFragment
-    private lateinit var favoritesFragment: FavoritesFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         setupBottomNavigationView()
-        setupFragments()
+        setupViewPager()
     }
 
-    private fun setupFragments() {
-        movieListFragment = MovieListFragment()
-        favoritesFragment = FavoritesFragment()
-        bottomNavigationView.selectedItemId = R.id.actionMovieList
+    private fun setupViewPager() {
+        nonSwipeableViewPager.adapter = ViewPagerAdapter(supportFragmentManager, 0)
     }
 
     private fun setupBottomNavigationView() {
         bottomNavigationView.setOnNavigationItemSelectedListener {
             when (it.itemId) {
-                R.id.actionMovieList -> handleFragments(movieListFragment)
-                R.id.actionFavorites -> handleFragments(favoritesFragment)
+                R.id.actionMovieList -> nonSwipeableViewPager.currentItem = 0
+                R.id.actionFavorites -> nonSwipeableViewPager.currentItem = 1
             }
             return@setOnNavigationItemSelectedListener true
-        }
-    }
-
-    private fun handleFragments(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.frameLayout, fragment).commit()
         }
     }
 
